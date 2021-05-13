b040d0dbd63403b7c88857b3ac17f9f035e64d98
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-05-13 11:27:49 +0200

    feat: add options and function

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
 #!/usr/bin/env node
 import { Command } from "commander";
+import { cliOptions } from "./options.interface";
+import { setup } from "./setup";
 const program = new Command();
 
 program
   .version("0.0.1")
   .description("sets up local stargate container")
+  .option("-u, --username <username>", "username for stargate", "cassandra")
+  .option("-p, --password <password>", "password for stargate", "cassandra")
+  .option(
+    "-a, --auth-url <authurl>",
+    "stargate Authurl",
+    "http://localhost:8081/v1/auth"
+  )
+  .option(
+    "-n, --namespace-url <namespaceurl>",
+    "stargate namespaceurl",
+    "http://localhost:8082/v2/schemas/namespaces"
+  )
+  .option("-k, --keyspace <keyspace>", "stargate keyspace to create")
   .parse(process.argv);
 
 if (process.argv.length < 3) {
   program.outputHelp();
 }
+
+const cliOptionsRaw: any = program.opts();
+
+const options: cliOptions = cliOptionsRaw;
+
+setup(options);
---
+++ src/options.interface.ts
@@ -0,0 +1,7 @@
+export interface cliOptions {
+  username: string;
+  password: string;
+  authUrl: string;
+  namespaceUrl: string;
+  keyspace: string;
+}
