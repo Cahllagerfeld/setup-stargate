174539b0088101b1dcd0850a8db9bb95d19177a2
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-05-15 08:59:17 +0200

    refactor: old logic to command

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,36 @@
 #!/usr/bin/env node
 import { Command } from "commander";
-import { cliOptions } from "./options.interface";
-import { setup } from "./setup";
+import { setDefaults, setupDefault } from "./setupDefault";
 const program = new Command();
 
+program.version("0.0.2").description("sets up local stargate container");
+
 program
-  .version("0.0.1")
-  .description("sets up local stargate container")
-  .option("-u, --username <username>", "username for stargate", "cassandra")
-  .option("-p, --password <password>", "password for stargate", "cassandra")
-  .option(
-    "-a, --auth-url <authurl>",
-    "stargate Authurl",
-    "http://localhost:8081/v1/auth"
-  )
-  .option(
-    "-n, --namespace-url <namespaceurl>",
-    "stargate namespaceurl",
-    "http://localhost:8082/v2/schemas/namespaces"
-  )
-  .option("-k, --keyspace <keyspace>", "stargate keyspace to create")
-  .parse(process.argv);
+  .command("advanced")
+  .alias("a")
+  .description("Advanced Setup")
+  .action(() => console.log("advanced"));
+
+program
+  .command("default <keyspace> [username] [password] [authUrl] [namespaceUrl]")
+  .alias("d")
+  .description("Default Setup")
+  .action(async (keyspace, username, password, authUrl, namespaceUrl) => {
+    const options = setDefaults(
+      username,
+      password,
+      authUrl,
+      namespaceUrl,
+      keyspace
+    );
+    await setupDefault(options);
+  });
+
+program.parse(process.argv);
 
 if (process.argv.length < 3) {
   program.outputHelp();
 }
 
-const cliOptionsRaw: any = program.opts();
-
-const options: cliOptions = cliOptionsRaw;
-
-setup(options);
-
-export * from "./setup";
+export * from "./setupDefault";
 export * from "./options.interface";
---
+++ src/setupDefault.ts
@@ -0,0 +1,59 @@
+import axios from "axios";
+import chalk from "chalk";
+import figlet from "figlet";
+import { setupStargateOptions } from "./options.interface";
+
+export async function setupDefault(options: setupStargateOptions) {
+  if (!options.keyspace) {
+    console.log(chalk.red("please define keyspace name"));
+    return;
+  }
+  console.log(
+    chalk.cyan(figlet.textSync("setup-stargate", { horizontalLayout: "full" }))
+  );
+  try {
+    console.log(chalk.cyan("Initialization started"));
+    const response = await axios.post(options.authUrl, {
+      username: options.username,
+      password: options.password,
+    });
+    const token = response.data.authToken;
+    console.log(chalk.cyan("Authentication successfull"));
+    await axios.post(
+      options.namespaceUrl,
+      {
+        name: options.keyspace,
+      },
+      {
+        headers: {
+          "X-Cassandra-Token": token,
+        },
+      }
+    );
+    console.log(
+      chalk.green(
+        `stargate for keyspace ${options.keyspace} initialized successfully`
+      )
+    );
+  } catch {
+    console.log(chalk.red("couldn't create stargate namespace"));
+  }
+}
+
+export function setDefaults(
+  username: string | undefined,
+  password: string | undefined,
+  authUrl: string | undefined,
+  namespaceUrl: string | undefined,
+  keyspace: string
+): setupStargateOptions {
+  return {
+    username: username ? username : "cassandra",
+    password: password ? password : "cassandra",
+    authUrl: authUrl ? authUrl : "http://localhost:8081/v1/auth",
+    namespaceUrl: namespaceUrl
+      ? namespaceUrl
+      : "http://localhost:8082/v2/schemas/namespaces",
+    keyspace: keyspace,
+  } as setupStargateOptions;
+}
