6df26040b93dc1f7e5775546ecfef9360051ef25
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-05-15 10:54:16 +0200

    feat: added eslint + prettier (#13)

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,30 @@
 #!/usr/bin/env node
-import chalk from "chalk";
-import { Command } from "commander";
-import clear from "clear";
-import figlet from "figlet";
-import { setDefaults, setupDefault } from "./setupDefault";
-import { setupAdvanced } from "./setupAdvanced";
+import chalk from 'chalk';
+import { Command } from 'commander';
+import clear from 'clear';
+import figlet from 'figlet';
+import { setDefaults, setupDefault } from './setupDefault';
+import { setupAdvanced } from './setupAdvanced';
 const program = new Command();
 
 clear();
-console.log(
-  chalk.cyan(figlet.textSync("setup-stargate", { horizontalLayout: "full" }))
-);
-program.version("0.0.2").description("sets up local stargate container");
+console.log(chalk.cyan(figlet.textSync('setup-stargate', { horizontalLayout: 'full' })));
+program.version('0.0.2').description('sets up local stargate container');
 
 program
-  .command("advanced")
-  .alias("a")
-  .description("Advanced Setup")
+  .command('advanced')
+  .alias('a')
+  .description('Advanced Setup')
   .action(async () => {
     await setupAdvanced();
   });
 
 program
-  .command("default <keyspace> [username] [password] [authUrl] [namespaceUrl]")
-  .alias("d")
-  .description("Default Setup")
+  .command('default <keyspace> [username] [password] [authUrl] [namespaceUrl]')
+  .alias('d')
+  .description('Default Setup')
   .action(async (keyspace, username, password, authUrl, namespaceUrl) => {
-    const options = setDefaults(
-      username,
-      password,
-      authUrl,
-      namespaceUrl,
-      keyspace
-    );
+    const options = setDefaults(username, password, authUrl, namespaceUrl, keyspace);
     await setupDefault(options);
   });
 
@@ -42,5 +34,5 @@ if (process.argv.length < 3) {
   program.outputHelp();
 }
 
-export * from "./setupDefault";
-export * from "./options.interface";
+export * from './setupDefault';
+export * from './options.interface';
--- src/setupAdvanced.ts
+++ src/setupAdvanced.ts
@@ -1,9 +1,9 @@
-import inquirer from "inquirer";
-import { questions } from "./inquirer";
-import { setupStargateOptions } from "./options.interface";
-import { setupDefault } from "./setupDefault";
+import inquirer from 'inquirer';
+import { questions } from './inquirer';
+import { setupStargateOptions } from './options.interface';
+import { setupDefault } from './setupDefault';
 
-export async function setupAdvanced() {
+export async function setupAdvanced(): Promise<void> {
   const answersRaw: any = await inquirer.prompt(questions);
   const answers: setupStargateOptions = answersRaw;
   await setupDefault(answers);
