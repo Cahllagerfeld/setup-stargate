a469deb402a2ec9faeb84c6ce29305b9b4ce6931
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-05-15 10:27:20 +0200

    feat: added advanced mode

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 #!/usr/bin/env node
+import chalk from "chalk";
 import { Command } from "commander";
+import clear from "clear";
+import figlet from "figlet";
 import { setDefaults, setupDefault } from "./setupDefault";
+import { setupAdvanced } from "./setupAdvanced";
 const program = new Command();
 
+clear();
+console.log(
+  chalk.cyan(figlet.textSync("setup-stargate", { horizontalLayout: "full" }))
+);
 program.version("0.0.2").description("sets up local stargate container");
 
 program
   .command("advanced")
   .alias("a")
   .description("Advanced Setup")
-  .action(() => console.log("advanced"));
+  .action(async () => {
+    await setupAdvanced();
+  });
 
 program
   .command("default <keyspace> [username] [password] [authUrl] [namespaceUrl]")
---
+++ src/setupAdvanced.ts
@@ -0,0 +1,10 @@
+import inquirer from "inquirer";
+import { questions } from "./inquirer";
+import { setupStargateOptions } from "./options.interface";
+import { setupDefault } from "./setupDefault";
+
+export async function setupAdvanced() {
+  const answersRaw: any = await inquirer.prompt(questions);
+  const answers: setupStargateOptions = answersRaw;
+  await setupDefault(answers);
+}
